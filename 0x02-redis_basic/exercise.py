2b56bda1723041eaf797113807276b452e2ccdbc
BeygonK <kipkoechbeygon@gmail.com>
2024-08-24 09:00:50 +0300

    count calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,6 +22,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in Redis with a random key and return the key.
 
